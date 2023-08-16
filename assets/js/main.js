cf7fe7257003225d540373ae417b771e6564c3c5
Shane Doyle <shanedoyle1989@gmail.com>
2023-08-16 18:33:35 +0100

    feat(assets/js/main.js): add DOM selectors

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// page sections
+const heroSection = document.querySelector("#hero-section");
+
+// navbar
+const navbar = document.querySelector(".navbar");
+const navbarMenu = document.querySelector(".navbar-menu");
+const navbarHamburger = document.querySelector(".navbar-hamburger");
+const navbarCloseBtn = document.querySelector(".navbar-menu-close-btn");
+const navbarMenuItems = document.querySelectorAll(".navbar-menu-item");
+const navbarMenuLinks = document.querySelectorAll(".navbar-menu-link");
+
+// testimonials
+const testimonials = document.querySelectorAll(".testimonial");
+const paginationDots = document.querySelectorAll(".pagination-dot");
+const paginationRightArrow = document.querySelector(".pagination-right-arrow");
+const paginationLeftArrow = document.querySelector(".pagination-left-arrow");
+
+// modal
+const modal = document.querySelector(".modal");
+const backdrop = document.querySelector(".backdrop");
+const modalContent = document.querySelector(".modal-content");
+const modalForm = document.querySelector(".modal-form");
+const modalFormMessage = document.querySelector(".form-message");
+
+// buttons
+const contactBtn = document.querySelector(".contact-btn");
+const ctaBtn = document.querySelector(".btn-cta");
+const modalCloseBtn = document.querySelector(".modal-close-btn");
